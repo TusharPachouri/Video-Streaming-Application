6d792ea94a86fc4e79a44615dc7dd8e364ba5e2a
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-02-05 18:49:49 +0530

    Utils

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,6 +3,6 @@ class ApiError {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
-        this.success = statusCode > 400;
+        this.success = statusCode > 400
     }
 }
