c1524d1be5f670953ff5f0067f45da8ed802b2cd
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-02-13 20:21:28 +0530

    Add subscription model

--- src/models/subscriber.models.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const subscriberSchema = new mongoose.Schema({
-
-})
-
-export const Subscriber = mongoose.model('Subscriber', subscriberSchema)
-
-// write a code for writing number table
