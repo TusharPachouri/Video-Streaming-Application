b445b822bef6434e9eeefa0080b93222ead45cf3
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-02-14 21:50:31 +0530

    Add route for root endpoint and configure Vercel deployment

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+app.use("/", (req, res) => {
+  res.json({ message: "hello from my side" });
+});
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -17,7 +20,6 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
 
 //import router:=
 import userRouter from "./routes/user.routes.js";
