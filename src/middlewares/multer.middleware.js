9e0bbee00e57726374d48abb098d084becbf9c38
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-02-20 21:17:30 +0530

    Update multer middleware configuration

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,13 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+  storage: multer.memoryStorage(),
+  // destination: function (req, file, cb) {
+  //   cb(null, "./public/temp");
+  // },
+  // filename: function (req, file, cb) {
+  //   cb(null, file.originalname);
+  // },
 });
 
 export const upload = multer({
