6106e5b2e15376eddaf68f4f2474bbf0bcfa2d60
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-03-04 22:13:38 +0530

    File added to vercel

--- src/app.js
+++ src/app.js
@@ -2,16 +2,14 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-app.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "javascriptFrontEnd.html");
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.error("Error sending file:", err);
-      res.status(404).send("File not found");
-    }
-  });
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', async(req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Home.html'));
 });
 app.use(
   cors({
